8eacc13b02aa9be0d7ac07a7847f7561fb1db0c1
ThakurASP <98098649+ThakurASP@users.noreply.github.com>
2022-03-01 09:53:30 +0530

    Update about.php

--- about.php
+++ about.php
@@ -34,7 +34,7 @@
 		              	<div>
 			                <h2 class="mb-4">Offering Reliable Hosting</h2>
 			              	<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean.</p>
-			                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nesciunt voluptate, quibusdam sunt iste dolores consequatur</p>
+			                <p>A healthy Change</p>
 				            </div>
 		              </div>
 
